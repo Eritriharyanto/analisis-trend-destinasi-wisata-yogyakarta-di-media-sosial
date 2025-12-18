c8c6bd826a12625cc8a3886f2488b8815f6bf3a5
Eritriharyanto <wildyoung737@gmail.com>
2025-12-18 12:18:20 +0700

    update

--- Frontend/src/components/DestinasiCard.jsx
+++ Frontend/src/components/DestinasiCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function DestinasiCard({ id, title, img, slug }) {
+export default function DestinasiCard({
+  id,
+  title,
+  img,
+  slug,
+  lokasi,
+  rating,
+  deskripsi,
+  kategori,
+}) {
   const imageSrc = img || "https://via.placeholder.com/300x200?text=No+Image";
 
   return (
@@ -10,6 +19,20 @@ export default function DestinasiCard({ id, title, img, slug }) {
 
       <div className='card-body'>
         <h5 className='card-title'>{title}</h5>
+
+        {/* Lokasi */}
+        <p className='lokasi mb-1'>📍 {lokasi}</p>
+
+        {/* Rating & Kategori */}
+        <div className='info d-flex justify-content-between mb-2'>
+          <span className='rating'>⭐ {rating}</span>
+          <span className='kategori badge bg-primary text-capitalize'>
+            {kategori}
+          </span>
+        </div>
+
+        {/* Deskripsi */}
+        <p className='deskripsi'>{deskripsi}</p>
 
         <Link
           to={`/destinasi/${slug}`}
--- Frontend/src/pages/Destinasi.jsx
+++ Frontend/src/pages/Destinasi.jsx
@@ -5,36 +5,66 @@ export default function Destinasi() {
   const list = [
     {
       id: "alkid",
+      lokasi: "kota yogyakarta",
+      kategori: "taman kota",
+      rating: 4.7,
+      deskripsi:
+        " Alun-alun yang terkenal dengan hiburan malam, rental gokar berlampu neon, kios camilan, bar, dan musik ceria.",
       slug: "alkid",
       title: "Alun-Alun Kidul",
       img: "/d3.jpg",
     },
     {
       id: "tamanpintar",
+      lokasi: "kota yogyakarta",
+      kategori: "museum sains",
+      rating: 4.5,
+      deskripsi:
+        "Pusat pendidikan anak-anak populer yang menawarkan berbagai pameran interaktif dan game ilmu pengetahuan.",
       slug: "tamanpintar",
       title: "Taman Pintar",
       img: "/tamanpintar.jpg",
     },
     {
       id: "merapi",
+      lokasi: "sleman",
+      kategori: "gunung berapi",
+      rating: 4.4,
+      deskripsi:
+        "Gunung berapi aktif setinggi 2.900 m dengan jalur pendakian ke puncak berasap & Taman Nasional di sekitarnya.",
       slug: "merapi",
       title: "Gunung Merapi",
       img: "/GunungMerapi.jpg",
     },
     {
       id: "prambanan",
+      lokasi: "kota yogyakarta",
+      kategori: "pura",
+      rating: 4.7,
+      deskripsi:
+        "Kompleks candi Hindu yang luas & dibangun pada abad ke-9 dengan struktur batu atas meruncing & patung.",
       slug: "prambanan",
       title: "Candi Prambanan",
       img: "/prambanan.jpg",
     },
     {
       id: "lagunapantaidepok",
+      lokasi: "bantul",
+      kategori: "muara",
+      rating: 4.6,
+      deskripsi:
+        "Pantai tenang dengan laguna, penyewaan perahu, area piknik, & warung yang menyajikan hidangan laut segar.",
       slug: "lagunapantaidepok",
       title: "Laguna Pantai Depok",
       img: "/lagunapantaidepok.webp",
     },
     {
       id: "gembiraloka",
+      lokasi: "kota yogyakarta",
+      kategori: "kebun binatang",
+      rating: 4.5,
+      deskripsi:
+        "Dibuka pada 1956, kebun binatang 21 hektare dengan kolam sentuh, memberi makan hewan, naik gajah & unta.",
       slug: "gembiraloka",
       title: "Gembira Loka",
       img: "/gembiraloka.jpg",
